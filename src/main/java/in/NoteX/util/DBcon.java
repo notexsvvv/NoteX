9b304069a2346c301af289d495c89856082950c6
notexsvvv <166230018+notexsvvv@users.noreply.github.com>
2024-04-10 08:45:54 +0530

    Update DBcon.java password change to root

--- src/main/java/in/NoteX/util/DBcon.java
+++ src/main/java/in/NoteX/util/DBcon.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBcon {
 	 private static final String URL = "jdbc:mysql://localhost:3306/NoteX";
 	 private static final String USERNAME = "root";
-	 private static final String PASSWORD = "6269";
+	 private static final String PASSWORD = "root";
     static {
         try {
             // Load the MySQL JDBC driver
